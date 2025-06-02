dc09425d1ef26e3005a7c7d98051c8f293131d8c
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-06-02 16:50:48 +0200

    Update cropping code

--- PdfCropping/pdf_crop.py
+++ PdfCropping/pdf_crop.py
@@ -2,11 +2,23 @@ import pymupdf as pdf
 
 # Crops unnecessary areas from the page
 
-doc = pdf.open("orig_text.pdf")
+doc = pdf.open("PdfCropping/Grundeinkommen.pdf")
 page = doc[0]
 
-width = page.rect.width
-height = page.rect.height
+# page.set_rotation(0) uncomment if getting MediaBox errors
 
-page.set_cropbox(pdf.Rect(0,0,width-50,height-25))
+def crop_vertical(page : pdf.Page):
+    width = page.rect.width
+    height = page.rect.height
+
+    page.set_cropbox(pdf.Rect(0,0,width-50,height-25))
+
+def crop_horizontal(page : pdf.Page):
+    width = page.rect.width
+    height = page.rect.height
+
+    page.set_cropbox(pdf.Rect(50,0,width,height-50))
+
+crop_vertical(page)
+
 doc.save("cropped.pdf")
